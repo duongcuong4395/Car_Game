2dec7c721c473a5663c92e07fc28816c5e00a8dc
duongcuong <dangduongcuong@gmail.com>
2016-07-23 11:52:53 +0700

    (Script) Draw Point and Way_Point

---
+++ EnemyScript/Way_Point_Managers.js
@@ -0,0 +1,16 @@
+﻿var lastPos:Vector3;
+
+function OnDrawGizmos(){
+
+	
+	var wayPoints = gameObject.GetComponentInChildren(Transform);
+
+	for (var wayPoint:Transform in wayPoints)
+	{
+		
+		Gizmos.DrawSphere (wayPoint.position,5);
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(wayPoint.position, lastPos);
+		lastPos = wayPoint.position;
+	}
+}
